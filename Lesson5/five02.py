9cdd48c145262976287457655ef7b89ebf8bba91
innocent <xuzin.2014@gmail.com>
2024-06-11 20:54:53 +0500

    кот

---
+++ Lesson5/five02.py
@@ -0,0 +1,10 @@
+n = int(input())
+found = False
+for i in range(n):
+    phrase = input()
+    if 'кот' in phrase or 'Кот' in phrase:
+        found = True
+if found == True:
+    print('МЯУ')
+else:
+    print('НЕТ')        
