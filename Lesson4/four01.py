08ef22ffba2c2183d0031a7497e7344065d88859
innocent <xuzin.2014@gmail.com>
2024-06-11 14:23:55 +0500

    !

---
+++ Lesson4/four01.py
@@ -0,0 +1,11 @@
+daytemp = []
+while True:
+    temp = float(input())
+    if temp >= 22.0:
+        break
+    daytemp.append(temp)
+print(len(daytemp)//7)
+    
+
+    
+ 
---
+++ Lesson4/four02.py
@@ -0,0 +1,4 @@
+phrase = input()
+count = int(input())
+for i in range(0,count):
+    print(phrase)
