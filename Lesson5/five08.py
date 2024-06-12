d954222732f4e0fe4da5cdddd3547d20eb6a739d
innocent <xuzin.2014@gmail.com>
2024-06-12 05:33:06 +0500

    cats

--- Lesson5/five08.py
+++ Lesson5/five08.py
@@ -2,19 +2,21 @@ line_number = 0
 found_kot = False
 num = 0
 firstnum = 0
+count = 0
 while True:
     line = input()  
     line_number += 1 
     if 'Кот' in line or 'кот' in line:  
         found_kot = True
         num = line_number 
+        count += 1
     if firstnum == 0:
         firstnum = num
     if line == 'СТОП' and found_kot == True:  
-        print(firstnum)
+        print( count, firstnum)
         break
     if line == 'СТОП' and found_kot == False:
-        print(-1,)
+        print(count, -1)
         break
 
     
