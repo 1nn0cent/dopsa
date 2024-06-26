52c66d91bb26886e65eae014938af9adf1bf392b
innocent <xuzin.2014@gmail.com>
2024-06-26 16:03:36 +0500

    messed up

---
+++ Lesson13/1302.py
@@ -0,0 +1,14 @@
+dict = {}
+for i in range(int(input())):
+    num, name = input().split()
+    if name in dict:
+        dict[name].append(num)
+    else:
+        dict[name] = [num]
+
+for i in range(int(input())):
+    name = input()
+    if name in dict:
+        print(*dict[name])
+    else:
+        print('Нет в телефонной книге')
