3b9e125d4c5a6ea980c2a03ff47aa3fdd8df0fa1
innocent <xuzin.2014@gmail.com>
2024-08-26 15:00:00 +0500

    fibonacci

---
+++ Lesson17/1704.py
@@ -0,0 +1,5 @@
+def continue_fibonacci_sequence(sequence, n):
+    for i in range(n):
+        next_element = sequence[-1] + sequence[-2]
+        sequence.append(next_element)
+        # seq = seq + [n_el] - создается новый список, а append добавляет новый элемент в настоящий список
