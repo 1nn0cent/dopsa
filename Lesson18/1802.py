712bccbe8a35a28b6dfa9982fe771b110a7a016a
innocent <xuzin.2014@gmail.com>
2024-08-26 15:57:46 +0500

    mail

---
+++ Lesson18/1802.py
@@ -0,0 +1,7 @@
+def letter(name, date, email, place="Нефтекамск"):
+    print(f"To:", email)
+    print(f"Здравствуйте,", name)
+    print(f"Были бы рады видеть вас на встрече начинающих программистов в {place}, которая пройдет {date}.")
+
+
+letter("Salavat", "26.08.2024", "sobaka@sobaka.gav")
