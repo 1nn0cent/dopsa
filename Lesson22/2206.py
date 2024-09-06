6fe8804ecd491b5e379d19ac1eaef6f9503c8617
innocent <xuzin.2014@gmail.com>
2024-09-06 11:10:46 +0500

    stronger password

---
+++ Lesson22/2206.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_password(m):
+    upper_case = [i for i in string.ascii_uppercase if i not in 'IO']
+    lower_case = [i for i in string.ascii_lowercase if i not in 'lo']
+    digits = list(string.digits[2:])
+    chars = upper_case + lower_case + digits
+
+    result = [random.choice(i) for i in (upper_case, lower_case, digits)]
+    result += [random.choice(chars) for i in range(m - 3)]
+    return ''.join(result)
+
+def main(n, m):
+    return [generate_password(m) for _ in range(n)]
+
+print("Случайный пароль из 7 символов:" , generate_password(7))
+print("10 случайных паролей длиной 15 символов:")
+print(*main(10, 15), sep="\n")
