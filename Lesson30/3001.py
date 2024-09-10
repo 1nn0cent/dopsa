40bd058ddb67760eac30054fd60f440bebadc8a8
innocent <xuzin.2014@gmail.com>
2024-09-10 18:36:26 +0500

    kc

---
+++ Lesson30/3001.py
@@ -0,0 +1,21 @@
+class FoodInfo(object):
+ 
+    def __init__ (self,prot, fats, carb):
+        self.prot = prot
+        self.fats = fats
+        self.carb = carb
+ 
+    def get_proteins(self):
+        return self.prot
+    
+    def get_fats(self):
+        return self.fats
+    
+    def get_carbohydrates (self):
+        return self.carb
+    
+    def get_kcalories(self):
+        return 4 * self.prot + 9 * self.fats + 4 * self.carb
+ 
+    def __add__ (self,value):
+        return FoodInfo(self.prot + value.prot, self.fats + value.fats, self.carb + value.carb)
